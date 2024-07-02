eb4aeaf6a5ad6cfd8bcb955c2647fca5ddc52a4e
Juan <juangaliano.18@hotmail.com>
2024-07-02 19:45:34 -0300

    commit init

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import conectardb from "./config/db.js";
+
+const app = express();
+
+dotenv.config();
+conectardb();
+
+console.log()
+
+app.use("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`servidor funcionando en el puerto ${PORT}`);
+});
